a311f636dfb3cfa883dcd57c6c738cc79ec906c8
volyx <volyihin@gmail.com>
2016-09-28 20:20:06 +0300

    add tests

--- src/test/java/io/github/volyx/RecordsFileTest.java
+++ src/test/java/io/github/volyx/RecordsFileTest.java
@@ -2,22 +2,36 @@ package io.github.volyx;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.UUID;
 
 public class RecordsFileTest {
 
+    private RecordsFile recordsFile;
+
+    @Before
+    public void before() throws IOException {
+        try {
+            recordsFile = new RecordsFile("testDatabase.jdb", "rw");
+        } catch (IOException e) {
+            recordsFile = new RecordsFile("testDatabase.jdb", 64);
+        }
+    }
+
     @Test
     public void test1() throws IOException, ClassNotFoundException {
-        RecordsFile recordsFile = new RecordsFile("testDatabase.jdb", 64);
-        RecordWriter rw = new RecordWriter("foo.lastAccessTime");
+
+        final String key = "foo.lastAccessTime-" + UUID.randomUUID().toString();
+        RecordWriter rw = new RecordWriter(key);
         Date date = new Date();
         rw.writeObject(date);
         recordsFile.insertRecord(rw);
 
-        RecordReader rr = recordsFile.readRecord("foo.lastAccessTime");
+        RecordReader rr = recordsFile.readRecord(key);
         Date d = (Date)rr.readObject();
 
         Assert.assertEquals(date, d);
